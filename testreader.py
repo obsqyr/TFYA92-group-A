445f47c193bb6ca14e2a94d49e66d37aabfe8f58
WSten <70895694+WSten@users.noreply.github.com>
2020-11-09 15:47:17 +0100

    Delete testreader.py
    Not necessary

--- testreader.py
+++
@@ -1,9 +0,0 @@
-def reader():
-    pos = open("Positions_example", "r")
-    poslist = pos.readlines()
-    print poslist[1]
-    print poslist[5]
-    print poslist[13]
-
-if __name__ == "__main__":
-    reader()
