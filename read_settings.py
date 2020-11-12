b87d55b3153d1d23af42c33d7e4a774fa9ec5921
William <wilst106@student.liu.se>
2020-11-12 16:07:24 +0100

    Added more settings, md.py now reads settings file to get (some of) the parameters of the simulations, added some comments

--- read_settings.py
+++ read_settings.py
@@ -7,10 +7,25 @@ def read_settings_file():
         # 'Default' value for parameter: try to find it in settings
         # file, otherwise default it
         try:
-            data['step_number']
+            data['time_step']
         except Exception as e:
-            data['step_number'] = 200
-            
+            data['time_step'] = 5
+        try:
+            data['max_steps']
+        except Exception as e:
+            data['max_steps'] = 200
+        try:
+            data['temperature']
+        except Exception as e:
+            data['temperature'] = 300
+        try:
+            data['ensemble']
+        except Exception as e:
+            data['ensemble'] = "NVE"
+        try:
+            data['friction']
+        except Exception as e:
+            data['friction'] = "0.001"
         return data
 
 if __name__ == "__main__":
