9b054b9e4096a89d2925b6bfd615bde0df9325d4
William <wilst106@student.liu.se>
2020-11-19 15:30:07 +0100

    .

--- testing.py
+++ testing.py
@@ -1,6 +1,7 @@
 import ase.io
 import math as m
 import numpy as np
+import ase.atoms
 from read_settings import read_settings_file
 from md import run_md
 from properties import initialize_properties_file
@@ -18,18 +19,16 @@ NACL = ase.io.read("nacl.cif", None)
 #print (y)
 #print (len(y))
 
-# Testing msd
+
 old_a = FaceCenteredCubic(directions=[[1, 0, 0], [0, 1, 0], [0, 0, 1]],
                           symbol="Ar",
                           latticeconstant = 5.256,
                           size=(6, 6, 6),
                           pbc=True)
 
-a = run_md()
+
+#a = run_md()
 
 #msqdisp = meansquaredisp(a, old_a)
 
 #print(msqdisp)
-
-initialize_properties_file(old_a)
-calc_properties(a, old_a)
