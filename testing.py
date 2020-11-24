21ea5f36989109c29e67c2536dccdadaaa764d4b
Emil <emiji263@student.liu.se>
2020-11-24 09:27:12 +0100

    ?

--- testing.py
+++ testing.py
@@ -2,13 +2,15 @@ import ase.io
 import math as m
 import numpy as np
 import ase.atoms
-from read_settings import read_settings_file
-from md import run_md
-from properties import initialize_properties_file
-from properties import calc_properties
+from asap3 import Trajectory
+#from read_settings import read_settings_file
+#from md import run_md
+#from properties import initialize_properties_file
+#from properties import calc_properties
 from ase.lattice.cubic import FaceCenteredCubic
+from visualization import visualize
 
-NACL = ase.io.read("nacl.cif", None)
+#NACL = ase.io.read("nacl.cif", None)
 
 #x = NACL.get_atomic_numbers()
 #print (x)
@@ -25,7 +27,6 @@ old_a = FaceCenteredCubic(directions=[[1, 0, 0], [0, 1, 0], [0, 0, 1]],
                           latticeconstant = 5.256,
                           size=(6, 6, 6),
                           pbc=True)
-
 
 #a = run_md()
 
--- visualization.py
+++ visualization.py
@@ -1,3 +1,3 @@
 def visualize(file, atoms):
-    traj = Trajectory(to.string(file), 'w', atoms)
+    traj = Trajectory(file, 'w', atoms)
     dyn.attach(traj.write, interval=1000)
