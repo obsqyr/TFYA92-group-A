7602812701ff7e9ac7d9cf45005779f6e7ba3652
Petter <petcy342@student.liu.se>
2020-12-04 14:54:44 +0100

    Created a new  branch

--- analysis.py
+++ analysis.py
@@ -10,22 +10,22 @@ def extract():
     def lj(str, k = d):
         return str.ljust(k+10)
 
-    file.write(lj("Material")+lj("MSD")+lj("Self_diff")+lj("Specific heat")+lj("Lattice constant"))
-    file.write(lj("Bulk modulus")+lj("Cohesive energy")+lj("Debye",2)+lj("Lindemann")+"\n")
+    file.write(lj("Material")+lj("MSD")+lj("Self_diff")+lj("Spec heat")+lj("Lattice C"))
+    file.write(lj("Bulk mod")+lj("Coh Energy")+lj("Debye",2)+lj("Lindemann")+"\n")
 
     for filename in os.listdir("property_calculations/"):
         if filename.startswith("properties_") and filename.endswith(".txt"):
             print("property_calculations/"+filename)
             f = open("property_calculations/"+filename, "r")
-            #print(f.read().split("\n")[-1])
             lines = f.read().split("\n")
-            mat = lines[2].split()[1]
-            print(mat)
-            #lastline = f.read().split("\n")[-1]
-            msd = lines[-1].split()[4]
-            selfd = lines[-1].split()[5]
-            Cv = lines[-1].split()[7]
-            file.write(lj(mat)+lj(msd)+lj(selfd)+lj(Cv))
+            if lines[-4] == "Time averages:":
+                mat = lines[2].split()[1]
+                print(mat)
+                msd = lines[-1].split()[4]
+                selfd = lines[-1].split()[5]
+                Cv = lines[-1].split()[7]
+                file.write(lj(mat)+lj(msd)+lj(selfd)+lj(Cv))
+                file.write("\n")
             f.close()
     file.close()
     return
