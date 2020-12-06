475be7f049298a41d57ab387646e5d70fd87b25d
Linda Le <attlevafritt@gmail.com>
2020-12-06 12:01:23 +0100

    Added a test script. Tested to create a mongodb database.

---
+++ test_make_mongodb.py
@@ -0,0 +1,16 @@
+# This is for testing to make a mongodb database
+
+import pymongo
+client = pymongo.MongoClient() # default host and port
+# Examples to specify host and port
+#client = MongoClient('localhost', 27017)
+#client = MongoClient('mongodb://localhost:27017/')
+
+db = client.test_database
+collection = db.test_collection
+post = {"author": "Linda",
+        "text": "This is a test to create a mongodb database.",
+        "tags": ["mongodb", "python"],
+        "date": "6 december"}
+post_id = db.posts.insert_one(post)
+print("This is the post_id: ", post_id)
