b5bbbc11ed31e4c8f86ea36b9635692ecb55cb63
Linda Le <attlevafritt@gmail.com>
2020-12-21 01:10:08 +0100

    changed name of file, and made some unittests for formula_anynomous functionalities.

---
+++ make_mongodb.py
@@ -0,0 +1,247 @@
+#!/usr/bin/env python3
+# Currently uses mongomock instead of pymonogo. Just change to pymongo for usage.
+import mongomock
+import os
+import re
+import warnings
+import json
+import datetime
+from bson.json_util import dumps
+import chemparse
+from string import ascii_uppercase, ascii_lowercase
+
+def extract_elements(pretty_formula):
+    """Returns a list of strings over elements pretty_formula consists of.
+
+    Paramters:
+    pretty_formula (str): A string over the pretty formula, empirical formula, over material.
+
+    Returns:
+    list: Returns a list of strings, consisting of elements from empirical formula.
+    """
+    dict = chemparse.parse_formula(pretty_formula)
+    elements_list = list(dict.keys())
+
+    return elements_list
+
+def calc_element_ratios(pretty_formula):
+    """ Calculates the ratio of unique elements in a material.
+
+    Paramters:
+    pretty_formula (str): A string over the pretty formula, empirical formula, over material.
+
+    Returns:
+    list: Returns a list of floats. These floats are element ratios for the elements given in the same
+            order as they occur in pretty_formula.
+    """
+    dict = chemparse.parse_formula(pretty_formula)
+    vals = list(dict.values())
+
+    res = []
+    for i in range(0, len(vals)):
+        ratio_i = vals[i]/sum(vals)
+        res.append(ratio_i)
+    return res
+
+def species_sites(pretty_formula):
+    """Outputs a list over species at sites.
+
+    Paramters:
+    pretty_formula (str): A string over the pretty formula, empirical formula, over material.
+
+    Returns:
+    list: Returns a list of strings. Where each strings are an element symbol for each sites given
+            by cartesian_site_positions.
+    """
+    dict = chemparse.parse_formula(pretty_formula)
+    key_list = list(dict.keys())
+    res_str = " "
+
+    for key_str in map(str, key_list):
+        cnt_element = dict[key_str]
+        duplicate_str = key_str + " "
+        extended_str = duplicate_str * int(cnt_element)
+        res_str = res_str + extended_str
+
+    res = res_str.split()
+    return res
+
+def anynomize_one_symbols(el_num_list):
+    """ Code a list of elements. Make the anynomous coding A,B,C,...Z followed by corresponding chemical
+        propotional number. So for the chemical Ag3Na2Mg4 get the coding A4B3C1.
+
+    Paramters:
+    el_num_list (list): A list of strings. Each string are numbers specifiying the number of
+                        atoms of specific spieces. For example for system Ag3Na2Mg4
+                        el_num_list should be ["4", "3", "2"].
+
+    Returns:
+    str: Returns a string of the anynomous formula, with proportion numbers, for the material.
+    """
+    # putting in "#"" to mark the end of string "ABCD..Z#"
+
+    for i in range(0, len(el_num_list)):
+        first = ascii_uppercase[i]
+        el_num = el_num_list[i]
+        if el_num == str(1): # if 1 then omitted.
+                el_num = ""
+                yield first + el_num
+        else:
+            yield first + el_num
+
+def anynomize_two_symbols(el_num_list):
+    """ Code a list of elements. Make the anynomous coding Aa, Ba, Ca,..Za, Ab, Bb,..Zb e.t.c
+        followed by corresponding chemical propotional number. This function is called only if the number of
+        elements exceeds 26. It's called after anynomize_one_symbols(el_num_list) to code anynomous formula
+        for the number 27th and above element.
+
+    Paramters:
+    el_num_list (list): A list of strings. Each string are numbers specifiying the number of
+                        atoms of specific spieces.
+
+    Returns:
+    str: Returns a string of the anynomous formula, of two symbols followed by proportion numbers for the material.
+    """
+    # putting in "#"" to mark the end of string "ABCD..Z#"
+    uppercase = ascii_uppercase + "#"
+    # Making codeing Aa, Ba, Ca,.. Za, Ab, Bb,...Zb etc.
+    ii = -1
+    for second in ascii_lowercase:
+        if ii == len(el_num_list):
+            break
+        for first in uppercase:
+            ii += 1
+            if first == "#": # symbol after Zx where x is a-z
+                ii = ii - 1
+                break
+            elif ii == len(el_num_list):
+                break
+            el_num = el_num_list[ii]
+            if el_num == str(1):
+                el_num = ""
+                yield first + second + el_num
+            else:
+                yield first + second + el_num
+
+
+
+def make_anonymous_form(pretty_formula):
+    """ Converts into formula anynomous.
+    Paramters:
+    pretty_formula (str): A string over the pretty formula, empirical formula, over material.
+
+    Returns:
+    str: Returns a string the anonymous formula. This is the pretty_formula but the elements are
+            first ordered by their chemical proportion number, and then, in order left to right,
+            replaced by anonymous symbols A, B, C, ..., Z, Aa, Ba, ..., Za, Ab, Bb, ... and so on.
+    """
+    dict = chemparse.parse_formula(pretty_formula)
+    values_list = list(dict.values())
+    sorted_values = sorted(values_list, reverse = True)
+    # Make into list of strings
+    # initially a list of floats so int(x) necessary
+    tmp_str = " ".join(str(int(x)) for x in sorted_values)
+    sorted_values = tmp_str.split()
+
+    # Make it anynomous
+    res_str_list = []
+    if len(sorted_values) > 26: # A-Z is 26 symbols in total
+        sorted_vals_1 = sorted_values[0:26]
+        sorted_vals_2 = sorted_values[26:len(sorted_values)]
+        for value in anynomize_one_symbols(sorted_vals_1):
+            res_str_list.append(value)
+        # after A,B,C...Z comes Aa, Ba, Ca,.. Za,.. e.t.c
+        for value in anynomize_two_symbols(sorted_values):
+            res_str_list.append(value)
+    else: # If less or equal to 26 symbols
+        for value in anynomize_one_symbols(sorted_values):
+            res_str_list.append(value)
+
+    res_str = "".join(res_str_list)
+    return res_str
+
+
+def make_MDdb():
+    """ Makes a mongodb database from results by running MD simulation.
+
+    Returns: Returns the database, in this case one collection, over the results.
+    """
+    md_runned = os.path.exists("property_calculations")
+
+    if not md_runned:
+        raise Exception("property_calculations folder doesn't exist.")
+
+    client = mongomock.MongoClient()
+    db = client.test_database
+    collection = db.posts
+
+    files = os.listdir("property_calculations") # How many files in dir
+    file_cnt = len(files)
+
+    for i in range(file_cnt):
+        file = open("property_calculations/properties_"+str(i)+".txt", "r")
+        lines = file.read().splitlines()
+        Id = lines[0].split(":")[1]
+        Id = int(Id)
+        unit_cell_comp = lines[1].split(":")[1]
+        system_name = lines[2].split(":")[1]
+        system_name = system_name.replace(" ", "") # remove white space
+        elements = extract_elements(system_name)
+        el_ratios = calc_element_ratios(system_name)
+        species_at_sites = species_sites(system_name)
+        formula_anonymous = make_anonymous_form(system_name)
+        #cartesian_site_pos = get_sites_pos()
+        #species = make_species_structure("d")
+
+        pattern = re.compile(r'Time averages:')
+        found = False
+        for i in range(len(lines)):
+            sentence = lines[i]
+            if re.search(pattern, sentence):
+                time_av_line = lines[i+3]
+                time_av_line = time_av_line.split()
+                dt = datetime.datetime.now()
+                found = True
+        if found: # only store in database if time average exist.
+            post = {"pretty_formula": system_name,
+                    "Unit Cell Composition": unit_cell_comp ,
+                    "Properties": {
+                                    "Epot [eV/atom]": float(time_av_line[0]),
+                                    "Ekin [eV/atom]": float(time_av_line[1]),
+                                    "Etot [eV/atom]": float(time_av_line[2]),
+                                    "Temp [K]": float(time_av_line[3]),
+                                    "MSD [Å^2]": float(time_av_line[4]),
+                                    "Self diffusion [Å^2/fs]": float(time_av_line[5]),
+                                    "Pressure [Pa]": float(time_av_line[6]),
+                                    "Specific_heat [eV/K]": float(time_av_line[7])},
+                    "last_modified": dt,
+                    "nperiodic_dimensions": 3, # work with cubic materials- HARD CODED
+                    "dimension_types": [1, 1, 1],
+                     "elements": elements,
+                     "elements_ratios": el_ratios ,
+                     "chemsys": "-".join(elements),
+                     "nelements": len(elements),
+                     "species_at_sites": species_at_sites,
+                     #"nsites": len(cartesian_site_pos),
+                     #"cartesian_site_positions": cartesian_site_pos
+                     "formula_anonymous": formula_anonymous
+                    }
+            collection.insert_one(post).inserted_id
+        else:
+            warnings.warn("No time averages over properties found.")
+    return collection
+
+def MDdb_to_json(collection):
+    path = "optimade-python-tools/optimade/server/data/MD_structures.json"
+    cursor = collection.find()
+    with open(path, 'w') as file:
+        file.write('[' + '\n')
+        for document in cursor:
+            file.write(dumps(document, indent = 3))
+            file.write(',')
+        file.write('\n' + ']')
+    return None
+
+if __name__ == "__main__":
+    db = make_MDdb()
+    MDdb_to_json(db)
--- test_make_mongodb.py
+++ test_make_mongodb.py
@@ -1,234 +1,79 @@
 #!/usr/bin/env python3
-# Currently uses mongomock instead of pymonogo. Just change to pymongo for usage.
-import mongomock
-import os
-import re
-import warnings
-import json
-import datetime
-from bson.json_util import dumps
-import chemparse
-from string import ascii_uppercase, ascii_lowercase
+import unittest
+from make_mongodb import *
 
-def extract_elements(pretty_formula):
-    """Returns a list of strings over elements pretty_formula consists of.
+# This module tests the script make_mongodb.py
 
-    Paramters:
-    pretty_formula (str): A string over the pretty formula, empirical formula, over material.
+class TestMakeMongodb(unittest.TestCase):
+    def test_anynomize_one_symbols(self):
+        res = []
+        for val in anynomize_one_symbols(["1"]):
+            res.append(val)
+        self.assertEqual(res, ["A"])
 
-    Returns:
-    list: Returns a list of strings, consisting of elements from empirical formula.
-    """
-    dict = chemparse.parse_formula(pretty_formula)
-    elements_list = list(dict.keys())
+        res = []
+        for val in anynomize_one_symbols(["2"]):
+            res.append(val)
+        self.assertEqual(res, ["A2"])
 
-    return elements_list
+        res = []
+        for val in anynomize_one_symbols(["10", "3", "1"]):
+            res.append(val)
+        self.assertEqual(res, ["A10", "B3", "C"])
 
-def calc_element_ratios(pretty_formula):
-    """ Calculates the ratio of unique elements in a material.
+    def test_anynomize_two_symbols(self):
+        res = []
+        for val in anynomize_two_symbols(["1"]):
+            res.append(val)
+        self.assertEqual(res, ["Aa"])
 
-    Paramters:
-    pretty_formula (str): A string over the pretty formula, empirical formula, over material.
+        res = []
+        for val in anynomize_two_symbols(["2"]):
+            res.append(val)
+        self.assertEqual(res, ["Aa2"])
 
-    Returns:
-    list: Returns a list of floats. These floats are element ratios for the elements given in the same
-            order as they occur in pretty_formula.
-    """
-    dict = chemparse.parse_formula(pretty_formula)
-    print("This is pretty_formula ", pretty_formula)
-    print("This is the dict ", dict)
-    vals = list(dict.values())
-
-    res = []
-    for i in range(0, len(vals)):
-        ratio_i = vals[i]/sum(vals)
-        res.append(ratio_i)
-    return res
-
-def species_sites(pretty_formula):
-    """Outputs a list over species at sites.
-
-    Paramters:
-    pretty_formula (str): A string over the pretty formula, empirical formula, over material.
-
-    Returns:
-    list: Returns a list of strings. Where each strings are an element symbol for each sites given
-            by cartesian_site_positions.
-    """
-    dict = chemparse.parse_formula(pretty_formula)
-    key_list = list(dict.keys())
-    res_str = " "
-
-    for key_str in map(str, key_list):
-        cnt_element = dict[key_str]
-        duplicate_str = key_str + " "
-        extended_str = duplicate_str * int(cnt_element)
-        res_str = res_str + extended_str
-
-    res = res_str.split()
-    return res
-
-def anynomize_one_symbols(el_num_list):
-    """ Code a list of elements. To have the anynomous formula.
-
-    Paramters:
-    el_num_list (list): A list of strings. Each string are numbers e.g. "10" over the number of
-                    atoms of specific spieces.
-
-    Returns:
-    str: Returns a string of the anynomous formula, with proportion numbers, for the material.
-    """
-    # putting in "#"" to mark the end of string "ABCD..Z#"
-
-    for i in range(0, len(el_num_list)):
-        print("Inside anynomize_one_symbol this is i ", i)
-        first = ascii_uppercase[i]
-        el_num = el_num_list[i]
-        if el_num == str(1): # if 1 then omitted.
-                el_num = ""
-                yield first + el_num
-        else:
-            yield first + el_num
-
-def anynomize_two_symbols(el_num_list):
-    # putting in "#"" to mark the end of string "ABCD..Z#"
-    uppercase = ascii_uppercase + "#"
-    # Making codeing Aa, Ba, Ca,.. Za, Ab, Bb,...Zb etc.
-    ii = -1
-    for second in ascii_lowercase:
-        if ii == len(el_num_list):
-            break
-        for first in uppercase:
-            ii += 1
-            if first == "#": # symbol after Zx where x is a-z
-                ii = ii - 1
-                break
-            elif ii == len(el_num_list):
-                break
-            el_num = el_num_list[ii]
-            if el_num == str(1):
-                el_num = ""
-                yield first + second + el_num
-            else:
-                yield first + second + el_num
+        res = []
+        for val in anynomize_two_symbols(["10", "3", "1"]):
+            res.append(val)
+        self.assertEqual(res, ["Aa10", "Ba3", "Ca"])
 
 
+        string = " 10 1 9 3 "*30 # over 120 numbers
+        str_list = string.split()
+        res = []
+        for value in anynomize_two_symbols(str_list):
+            res.append(value)
 
-def make_anonymous_form(pretty_formula):
-    """ Converts into formula anynomous.
-    Paramters:
-    pretty_formula (str): A string over the pretty formula, empirical formula, over material.
+        expected_res = ['Aa10','Ba','Ca9','Da3','Ea10','Fa','Ga9','Ha3','Ia10',
+                        'Ja','Ka9','La3','Ma10','Na','Oa9','Pa3','Qa10','Ra',
+                        'Sa9','Ta3','Ua10','Va','Wa9','Xa3','Ya10','Za','Ab9',
+                        'Bb3','Cb10','Db','Eb9','Fb3','Gb10','Hb','Ib9','Jb3',
+                        'Kb10','Lb','Mb9','Nb3','Ob10','Pb','Qb9','Rb3','Sb10',
+                        'Tb','Ub9','Vb3','Wb10','Xb','Yb9','Zb3','Ac10','Bc','Cc9',
+                        'Dc3','Ec10','Fc','Gc9', 'Hc3','Ic10','Jc','Kc9','Lc3','Mc10',
+                        'Nc','Oc9','Pc3','Qc10','Rc','Sc9','Tc3','Uc10','Vc','Wc9','Xc3',
+                        'Yc10','Zc','Ad9','Bd3','Cd10','Dd','Ed9','Fd3','Gd10','Hd',
+                        'Id9','Jd3','Kd10','Ld','Md9','Nd3','Od10','Pd','Qd9','Rd3','Sd10',
+                        'Td','Ud9','Vd3','Wd10','Xd','Yd9','Zd3','Ae10','Be','Ce9','De3','Ee10',
+                        'Fe','Ge9','He3','Ie10','Je','Ke9','Le3','Me10','Ne','Oe9','Pe3']
 
-    Returns:
-    str: Returns a string the anonymous formula. This is the pretty_formula but the elements are
-            first ordered by their chemical proportion number, and then, in order left to right,
-            replaced by anonymous symbols A, B, C, ..., Z, Aa, Ba, ..., Za, Ab, Bb, ... and so on.
-    """
-    dict = chemparse.parse_formula(pretty_formula)
-    values_list = list(dict.values())
-    sorted_values = sorted(value_list, reverse = True)
+        self.assertEqual(res, expected_res)
 
-    # Make it anynomous
-    res_str = []
-    if len(sorted_values) > 26: # A-Z is 26 symbols in total
-        sorted_vals_1 = sorted_values[0:26]
-        print("This is sorted_vals 1 ", sorted_vals_1)
-        sorted_vals_2 = sorted_values[26:len(sorted_values)]
-        print("This is sorted_vals 1 ", sorted_vals_2)
-        for value in anynomize_one_symbols(sorted_vals_1):
-            res_str.append(value)
-        # after A,B,C...Z comes Aa, Ba, Ca,.. Za,.. e.t.c
-        for value in anynomize_two_symbols(sorted_values):
-            res_str.append(value)
-    else: # If less or equal to 26 symbols
-        for value in anynomize_one_symbols(sorted_values):
-            res_str.append(value)
+    def test_make_anonymous_form(self):
+        res_str = make_anonymous_form("H2O")
+        self.assertEqual(res_str, "A2B")
 
-    return res_str
+        res_str = make_anonymous_form("Li6Na4")
+        self.assertEqual(res_str, "A6B4")
 
+        res_str = make_anonymous_form("Li3Na4Mg")
+        self.assertEqual(res_str, "A4B3C")
 
-def make_MDdb():
-    """ Makes a mongodb database from results by running MD simulation.
+        # Just hypothetical material of 30 different elements
+        string = "Na3Mg5Al"
+        res_str = make_anonymous_form(string)
+        expected_res = "A5B3C"
+        self.assertEqual(res_str, expected_res)
 
-    Returns: Returns the database, in this case one collection, over the results.
-    """
-    md_runned = os.path.exists("property_calculations")
-
-    if not md_runned:
-        raise Exception("property_calculations folder doesn't exist.")
-
-    client = mongomock.MongoClient()
-    db = client.test_database
-    collection = db.posts
-
-    files = os.listdir("property_calculations") # How many files in dir
-    file_cnt = len(files)
-
-    for i in range(file_cnt):
-        file = open("property_calculations/properties_"+str(i)+".txt", "r")
-        lines = file.read().splitlines()
-        Id = lines[0].split(":")[1]
-        Id = int(Id)
-        unit_cell_comp = lines[1].split(":")[1]
-        system_name = lines[2].split(":")[1]
-        system_name = system_name.replace(" ", "") # remove white space
-        elements = extract_elements(system_name)
-        el_ratios = calc_element_ratios(system_name)
-        species_at_sites = species_sites(system_name)
-        formula_anonymous = make_anonymous_form(system_name)
-        #cartesian_site_pos = get_sites_pos()
-        #species = make_species_structure("d")
-
-        pattern = re.compile(r'Time averages:')
-        found = False
-        for i in range(len(lines)):
-            sentence = lines[i]
-            #print("This is the sentence ", lines[i])
-            if re.search(pattern, sentence):
-                time_av_line = lines[i+3]
-                time_av_line = time_av_line.split()
-                dt = datetime.datetime.now()
-                found = True
-        if found: # only store in database if time average exist.
-            post = {"pretty_formula": system_name,
-                    "Unit Cell Composition": unit_cell_comp ,
-                    "Properties": {
-                                    "Epot [eV/atom]": float(time_av_line[0]),
-                                    "Ekin [eV/atom]": float(time_av_line[1]),
-                                    "Etot [eV/atom]": float(time_av_line[2]),
-                                    "Temp [K]": float(time_av_line[3]),
-                                    "MSD [Å^2]": float(time_av_line[4]),
-                                    "Self diffusion [Å^2/fs]": float(time_av_line[5]),
-                                    "Pressure [Pa]": float(time_av_line[6]),
-                                    "Specific_heat [eV/K]": float(time_av_line[7])},
-                    "last_modified": dt,
-                    "nperiodic_dimensions": 3, # work with cubic materials- HARD CODED
-                    "dimension_types": [1, 1, 1],
-                     "elements": elements,
-                     "elements_ratios": el_ratios ,
-                     "chemsys": "-".join(elements),
-                     "nelements": len(elements),
-                     "species_at_sites": species_at_sites,
-                     #"nsites": len(cartesian_site_pos),
-                     #"cartesian_site_positions": cartesian_site_pos
-                     "formula_anonymous": formula_anonymous
-                    }
-            collection.insert_one(post).inserted_id
-        else:
-            warnings.warn("No time averages over properties found.")
-    return collection
-
-def MDdb_to_json(collection):
-    path = "optimade-python-tools/optimade/server/data/MD_structures.json"
-    cursor = collection.find()
-    with open(path, 'w') as file:
-        file.write('[' + '\n')
-        for document in cursor:
-            file.write(dumps(document, indent = 3))
-            file.write(',')
-        file.write('\n' + ']')
-    return None
-
-if __name__ == "__main__":
-    db = make_MDdb()
-    MDdb_to_json(db)
+if __name__ == '__main__':
+    unittest.main()
